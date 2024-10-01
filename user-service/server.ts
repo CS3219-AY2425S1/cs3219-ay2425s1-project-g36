fe7b7642af5b3bc325698b2ca7528cf7ffb75802
larrywang0701 <113608053+larrywang0701@users.noreply.github.com>
2024-10-01 15:41:48 +0800

    Add cors headers into user service backend

--- user-service/server.ts
+++ user-service/server.ts
@@ -2,6 +2,7 @@ import express, { Application, Request, Response } from 'express'
 import connectMongoDB from './db/connectDB';
 import { PORT } from './utils/config'
 import authenticationRoute from './src/routes/authenticationRoute'
+import cors from 'cors'
 
 const app: Application = express();
 const port: string | number  = PORT;
@@ -12,9 +13,16 @@ app.get('/', (req: Request, res: Response) => {
     })
 })
 
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions))
+app.use('/authentication', authenticationRoute)
+
 app.listen(port, () => {
     console.log(`Server is running on port ${PORT}`);
     connectMongoDB();
 });
 
-app.use('/authentication', authenticationRoute)
